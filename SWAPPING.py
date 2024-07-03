64b680ddd30f4d8e7919e8fedd7f85c5fb6e2d55
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-04 00:34:50 +0530

    swapping programms

---
+++ SWAPPING.py
@@ -0,0 +1,16 @@
+A=30
+B=40
+A=A+B
+B=A-B
+A=A-B
+print("Swapped A value ",A)
+print("Swapped B value ",B)
+print()
+#method three
+C=50
+D=60
+C=C*D
+D=C/D
+C=C/D
+print("Swapped C value ",C)
+print("Swapped D value ",D)
