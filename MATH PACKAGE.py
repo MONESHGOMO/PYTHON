549215c15460fbbff50fedcb58411737a3666796
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-04 20:18:27 +0530

    BASIC MATH LIBRARY IMPLEMENTATION

---
+++ MATH PACKAGE.py
@@ -0,0 +1,25 @@
+import math
+
+a=math.sqrt(20)
+print(a)
+#floor key ,
+b=math.floor(2.9)
+print(b)
+#ceil key
+c=math.ceil(2.1)
+print(c)
+#power key
+d=math.pow(10,2)
+print(d)
+#calculating distance between two
+x1=10
+x2=20
+y1=30
+y2=40
+dx=x2-x1
+dy=y2-y1
+dy=math.pow(dy,2)
+dx=math.pow(dx,2)
+equation=dx+dy
+solution=math.sqrt(equation)
+print(solution)
