0dd859d6123405571e9f1fe1839fcef67ad4288f
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-06 14:26:45 +0530

    Add files via upload
    find lenght  of the array

---
+++ FIND_LEN_OF_THE_ARRAY.py
@@ -0,0 +1,11 @@
+#apporach one to find the length of the list
+mylist=[1,2,34,2,32,33]
+print(mylist)
+
+count=0
+for i in mylist:
+    count+=1
+print(count)
+print()
+#seconnd approach as simple we  use the buildin function
+print(len(mylist))
