ae2580f103bdd878495a45981019b1c5b0fc96e0
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-06 14:30:14 +0530

    Add files via upload
    sum of the elements in the array

---
+++ sum_of_Array.py
@@ -0,0 +1,8 @@
+#using the buildin  function
+
+Array=[1,2,3,4,5]
+print(sum(Array))
+
+print()
+print(sum(Array,10)) #this print statement add 10 to the sum of the array
+print(sum(Array,-10))#this print statement subtract 10 in the sum of the array
