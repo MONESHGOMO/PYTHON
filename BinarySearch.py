63847981d80465ed92370206367918b851760176
Monesh_programmer <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-26 22:10:23 +0530

    binary search

---
+++ BinarySearch.py
@@ -0,0 +1,28 @@
+#binary search
+
+
+'''
+
+https://www.javatpoint.com/binary-search-in-java
+
+'''
+a=[5, 6, 7, 8, 9, 10]
+target=8
+
+
+start=0ss
+end=len(a)-1
+
+while start <= end :
+    mid=start+(end-start)//2
+
+    if a[mid] == target:
+        print(mid)
+    elif a[mid]<target:
+        start=mid+1
+    elif a[mid]>target:
+        end=mid-1
+
+    else:
+        print("-1")
+
